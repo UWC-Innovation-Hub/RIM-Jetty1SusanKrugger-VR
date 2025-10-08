529a10e6e15c4b0d7fa1dc4099d1fdb811f410e4
Kyle Marais <17299097+TiltedDeveloper@users.noreply.github.com>
2025-10-08 12:02:48 +0200

    Small changes for gesture recognition sample

--- Assets/Scripts/MAIO_Vid_Controller.cs
+++ Assets/Scripts/MAIO_Vid_Controller.cs
@@ -1,16 +1,35 @@
+using Unity.VRTemplate;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class MAIO_Vid_Controller : MonoBehaviour
 {
+    public VideoPlayer VP;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
+    
 
-    // Update is called once per frame
-    void Update()
+    public void playVid()
     {
-        
+        if (VP!=null) 
+        {
+            VP.Play();
+        }
     }
+
+    public void pauseVid()
+    {
+        if (VP != null)
+        {
+            VP.Pause();
+        }
+    }
+
+
+
+
 }
